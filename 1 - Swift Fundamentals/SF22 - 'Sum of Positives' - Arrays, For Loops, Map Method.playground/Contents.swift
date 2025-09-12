d2bf52c756ce355e7289ad3a224648cda67fa961
Andrew Vander Wilt <andyvanderwilt@gmail.com>
2025-09-12 08:50:15 -0600

    Code challenge SF22

--- 1 - Swift Fundamentals/SF22 - 'Sum of Positives' - Arrays, For Loops, Map Method.playground/Contents.swift
+++ 1 - Swift Fundamentals/SF22 - 'Sum of Positives' - Arrays, For Loops, Map Method.playground/Contents.swift
@@ -15,4 +15,32 @@
 //  ⌺ Black Diamond Challenge:
     //  Create a second function that instead returns two arrays, separating the positive and the negative numbers in the array. You can return it as an ([Int], [Int]) tuple, or as a custom structure holding both arrays.
 
+func addPositives(array: [Int]) -> Int? {
+    var sum = 0
+    for int in array {
+        if int > 0 {
+            sum += int
+        }
+    }
+    return sum
+}
+
+if let result = addPositives(array: [1,4,-5,6]) {
+    print(result)
+}
+
+func blackDiamond(array: [Int]) -> ([Int] , [Int]) {
+    var posArray: [Int] = []
+    var negArray: [Int] = []
+    
+    for int in array {
+        if int > 0 {
+            posArray.append(int)
+        } else if int < 0 {
+            negArray.append(int)
+        }
+    }
+    return (posArray, negArray)
+}
+
 import Foundation
