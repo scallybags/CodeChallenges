a651ace3e51e20dc6185ee743596b24475b68746
Andrew Vander Wilt <andyvanderwilt@gmail.com>
2025-09-18 09:48:55 -0600

    did sf25

--- 1 - Swift Fundamentals/Complete/SF25 - 'Find Non Consecutive Elements' - Arrays, For Loops, Index Subscripting.playground/Contents.swift
+++ 1 - Swift Fundamentals/Complete/SF25 - 'Find Non Consecutive Elements' - Arrays, For Loops, Index Subscripting.playground/Contents.swift
@@ -6,7 +6,22 @@
     //  Your task is to find the first element of an array that is not consecutive.
     //  By "not consecutive," we mean not exactly 1 larger than the previous element of the array.
     //  If the whole array is consecutive, then return nil.
-
+func checkForDuplicates(array: [Int]) -> Int? {
+    var n: Int = 0
+    for i in array {
+        print(i)
+        if i == n + 1 {
+            n += 1
+        } else {
+            return i
+        }
+    }
+    return nil
+}
+var arr = [-1,1]
+if let v = checkForDuplicates(array: arr) {
+    print("result: \(v)")
+}
 //  Restrictions:
     //  The array will always have at least 2 elements and all elements will be numbers.
     //  The numbers will also all be unique and in ascending order.
@@ -21,5 +36,4 @@
     //  Create another function. This time, when a non-consecutive number is found, end the previous array and start a new one. Return an array of arrays of Integers where all the arrays of Integers now only have consecutive numbers.
         //  Input: [1,2,3,5,6,7,9,10,11]
         //  Output: [[1,2,3], [5,6,7], [9,10,11]]
-
 import Foundation
